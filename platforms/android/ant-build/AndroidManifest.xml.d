70cf884d173e87b2aee1fd4cd37a9ef5722628bd
silsgah <silasgah@yahoo.co.uk>
2014-12-08 04:15:12 -0800

    First
    Round commitment

---
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Mobile\kwamena\platforms\android\ant-build\AndroidManifest.xml : \
+C:\Mobile\kwamena\platforms\android\AndroidManifest.xml \
---
+++ platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+C:\Mobile\kwamena\platforms\android\ant-build\CordovaApp-debug-unaligned.apk : \
+C:\Mobile\kwamena\platforms\android\ant-build\CordovaApp.ap_ \
+C:\Mobile\kwamena\platforms\android\ant-build\classes.dex \
+C:\Mobile\kwamena\platforms\android\CordovaLib\ant-build\classes.jar \
