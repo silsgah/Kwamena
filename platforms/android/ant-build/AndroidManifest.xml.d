0cfca4c65aae10b7f7065b0a33040ed6473894ae
silsgah <silasgah@yahoo.co.uk>
2014-12-08 04:32:59 -0800

    Revert "First"
    This reverts commit 70cf884d173e87b2aee1fd4cd37a9ef5722628bd.

--- platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Mobile\kwamena\platforms\android\ant-build\AndroidManifest.xml : \
-C:\Mobile\kwamena\platforms\android\AndroidManifest.xml \
--- platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-C:\Mobile\kwamena\platforms\android\ant-build\CordovaApp-debug-unaligned.apk : \
-C:\Mobile\kwamena\platforms\android\ant-build\CordovaApp.ap_ \
-C:\Mobile\kwamena\platforms\android\ant-build\classes.dex \
-C:\Mobile\kwamena\platforms\android\CordovaLib\ant-build\classes.jar \
